15d846fb2775e4fedcc46cb467304fde95077bbe
mahbeb <linbub007@gmail.com>
2017-02-27 09:47:35 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/tugas3.java
@@ -0,0 +1,15 @@
+public class tugas3{
+    
+    public static void main(String []args){
+        int x = 3;
+        int y = Angka(x);
+        System.out.println("masukkan Angka Anda : " + y);
+    
+    }
+    
+    public static int Angka(int x1){
+        int angka = x1;
+        
+        return angka ;
+    }
+    }
