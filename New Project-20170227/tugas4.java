4f3af5064fee88ce35276f896f20b35f2ef59274
mahbeb <linbub007@gmail.com>
2017-02-27 10:07:39 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/tugas4.java
@@ -0,0 +1,12 @@
+public class tugas4{
+    
+    public static void main(String []args){
+        int param = 2;
+        int hasil = Penjumlahan(param);
+        System.out.println("Angka penjumlahan " + param + " + 1 = " + hasil);
+    }
+    public static int Penjumlahan (int a1) {
+        int angka = a1;
+        return angka + 1;
+    }
+}
