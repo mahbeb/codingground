647e9702a21e352869abfa94ea79d6b482f19770
mahbeb <linbub007@gmail.com>
2017-02-27 09:45:58 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/tugas2.java
@@ -0,0 +1,12 @@
+    public class tugas2 {
+        
+      public static void main( String[] args )
+   {
+   String[] nama = new String []{
+       "Muhammad","Fathul","Mahbub"
+   };
+   System.out.println(nama[0]);
+   System.out.println(nama[1]);
+   System.out.println(nama[2]);
+   }
+}
