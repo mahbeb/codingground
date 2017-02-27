6722aa0f06442e4e8d5dc9a5eb53b171427ab006
mahbeb <linbub007@gmail.com>
2017-02-27 10:07:39 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/tugas1.java
@@ -0,0 +1,8 @@
+public class tugas1{
+    
+    public static void main (String []args){
+        String nama ="mahbub";
+        String kota ="Jepara";
+        System.out.println("Nama : " + nama + "\n" + "Kota : " + kota);
+    }
+}
