a65140cf97a4b51214b01fbebf1876a8214b3108
mahbeb <linbub007@gmail.com>
2017-03-05 21:31:44 +0700

    Created by Coding Ground Client

---
+++ New Project-20170305/Tugas11/Mahasiswa.java
@@ -0,0 +1,8 @@
+public class Mahasiswa extends Biodata11{
+	String nim = "141240000284";
+	String jurusan = "Teknik Informatika";
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur + "\nNIM : " + nim + "\nJurusan : " + jurusan);
+	}
+}
