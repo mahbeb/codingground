1244b09a663ea60ea96b4851f59d314c856d1fe9
mahbeb <linbub007@gmail.com>
2017-03-05 21:11:14 +0700

    Created by Coding Ground Client

---
+++ New Project-20170305/Tugas8/Biodata.java
@@ -0,0 +1,9 @@
+public class Biodata{
+	String nama = "Fathul Mahbub";
+	String alamat = "Jepara";
+	int umur = 22;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
